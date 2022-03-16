8af6222b43d8ddb906324f70e9c4c4743b6f1d83
sangeet2020 <15uec053@lnmiit.ac.in>
2022-03-16 11:02:31 +0100

    something buggy, not fixed yet

--- model/model.py
+++ model/model.py
@@ -1,8 +1,53 @@
+import pdb
+import math
+
 from turtle import forward
 import torch
 import torch.nn as nn
 
 from transformers import AutoModel, BertModel
+
+
+class MultiHeadAttention(nn.Module):
+    """Multi-head attention class where query is encoded 
+    slot type and keys and values are encoded user's utterance.
+    """
+    
+    def __init__(self, hid_dim, heads, dropout=None):
+        super().__init__()
+        self.hid_dim = hid_dim
+        self.heads = heads
+        
+        assert self.hid_dim % self.heads == 0
+        self.d_k = self.hid_dim // self.heads
+        
+        self.W_q = nn.Linear(self.hid_dim, self.hid_dim)
+        self.W_k = nn.Linear(self.hid_dim, self.hid_dim)
+        self.W_v = nn.Linear(self.hid_dim, self.hid_dim)
+        self.out = nn.Linear(self.hid_dim, self.hid_dim)
+        
+        self.dropout = self.dropout = nn.Dropout(dropout)
+        
+        
+    def forward(self, Q, K, V, mask=None):
+        batch_size = Q.shape[0]
+        
+        Q = self.W_q(Q).view(batch_size, -1, self.heads, self.d_k).transpose(1, 2)  # query = [bs, num_heads, query_len, d_k]
+        K = self.W_k(K).view(batch_size, -1, self.heads, self.d_k).transpose(1, 2)  # key   = [bs, num_heads, key_len, d_k]
+        V = self.W_v(V).view(batch_size, -1, self.heads, self.d_k).transpose(1, 2)  # value = [bs, num_heads, value_len, d_k]
+        
+        # size = [bs, num_heads, query_len, key_len]
+        scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+        if mask is not None:
+            scores = scores.masked_fill(mask==0, -1e9)
+        
+        self.attn_scores = torch.softmax(scores, dim=-1)
+        X = torch.matmul(self.dropout(self.attn_scores), V)  # size = [bs, num_heads, query_len, key_len] = [32, 8, 1, 50]
+        
+        X = X.transpose(1, 2).contiguous().view(batch_size, -1, self.hid_dim)
+        self.X = self.out(X)    # [32, 1, 768]
+        return self.attn_scores, self.X 
+        
 
 
 class OurModel(nn.Module):
@@ -20,15 +65,36 @@ class OurModel(nn.Module):
         self.utt_encoder = AutoModel.from_pretrained(config["span_bert_model"])
 
         # MultiHead attention component
+        self.bert_output_dim = self.utt_encoder.config.hidden_size  # 768
+        self.heads = config["heads"]    # number of heads in multi-headed attention
+        self.mha = MultiHeadAttention(self.bert_output_dim, self.heads, dropout=config["dropout"])
 
         # RNN for BIO tagging component
+        self.hidden_dim = config["hidden_dim"]
+        self.rnn_num_layers = config["rnn_num_layers"]
+        self.hidden_dropout_prob = self.utt_encoder.config.hidden_dropout_prob
+
+        self.rnn = nn.LSTM(input_size=self.bert_output_dim,     # 768
+                            hidden_size=self.hidden_dim,        # 100, 200, 300
+                            num_layers=self.rnn_num_layers,     # 1
+                            dropout=self.hidden_dropout_prob,   
+                            batch_first=True)
+        self.init_parameter(self.rnn)
+        
+        # self.linear = nn.Linear(self.hidden_dim, config["utt_max_len"])
+        # self.layer_norm = nn.LayerNorm(config["utt_max_len"])
+        self.slot_dim = config["slot_max_len"]
+        self.linear = nn.Linear(self.hidden_dim, self.bert_output_dim)
+        self.layer_norm = nn.LayerNorm(self.bert_output_dim)
+        self.dropout = nn.Dropout(self.hidden_dropout_prob)
+        self.sigmoid = nn.Sigmoid()
     
     def forward(self, slot, slot_attn_mask, utt, utt_attn_mask):
         # slot shape: [32, 1, 5]
         # slot_attn_mask shape: [32, 1, 5]
         # utt shape: [32, 1, 50]
         # utt_attn_mask shape: [32, 1, 50]
-
+        
         # === Get a single BERT contextualised vector for a slot ===
         # Remove dimension 1 since BERT model expects [B(atch), N(umber of tokens)] shape
         slot = slot.squeeze(dim=1)
@@ -51,6 +117,32 @@ class OurModel(nn.Module):
 
         # === Apply MultiHead attention between slot vector and utterance's tokens
         # Query is slot vector, while keys and values are utterance's tokens vectors
-
+        Q = slot_vector
+        K = utt_vectors
+        V = utt_vectors
+        attn_scores, X, = self.mha(Q, K, V, mask=None)     # X = [32, 1, 768]
+                
         # === Use RNN to output utterance tagged using BIO format ===
+        # set initial hidden of rnns zero
+        input_ids = slot
+        pdb.set_trace()
+        h = torch.zeros(self.rnn_num_layers, input_ids.shape[0] * self.slot_dim, self.hidden_dim)    # [1, 32, 100]
+        c = torch.zeros(self.rnn_num_layers, input_ids.shape[0] * self.slot_dim, self.hidden_dim)    # [1, 32, 100]
+        rnn_out, _ = self.rnn(X, (h, c))
+        
+
+        rnn_out = self.layer_norm(self.linear(self.dropout(rnn_out)))   # rnn_out = [32, 1, 50]        
+        hidden = rnn_out.view(self.slot_dim, input_ids.shape[0], input_ids.shape[1], -1)
+        hidden = self.sigmoid(hidden)
+        pdb.set_trace()
+        return hidden
+        
+
+                
+    @staticmethod
+    def init_parameter(module):
+        torch.nn.init.xavier_normal_(module.weight_ih_l0)
+        torch.nn.init.xavier_normal_(module.weight_hh_l0)
+        torch.nn.init.constant_(module.bias_ih_l0, 0.0)
+        torch.nn.init.constant_(module.bias_hh_l0, 0.0)
 
--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import time
+import pdb
 
 from tqdm import tqdm
 
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from model.model import OurModel
@@ -10,7 +13,7 @@ from utils.util import open_file, set_seed, epoch_time
 from data.restaurant_dataset import Restaurant8kDataset
 
 
-def train_epoch(model, train_set, batch_size, use_cuda):
+def train_epoch(model, criterion, optimizer, train_set, batch_size, use_cuda):
     data_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
 
     epoch_loss = 0
@@ -20,11 +23,14 @@ def train_epoch(model, train_set, batch_size, use_cuda):
         slots_attn_masks = batch[1].cuda() if use_cuda else batch[1]
         encoded_utts = batch[2].cuda() if use_cuda else batch[2]
         utts_attn_masks = batch[3].cuda() if use_cuda else batch[3]
-        outputs = batch[4].cuda() if use_cuda else batch[4]
-
-        output = model(encoded_slots, slots_attn_masks, encoded_utts, utts_attn_masks)
-
+        true_labels = batch[4].cuda() if use_cuda else batch[4]     # [32, 1, 50]
+        
         # Training step
+        output = model(encoded_slots, slots_attn_masks, encoded_utts, utts_attn_masks)
+        loss = criterion(output, true_labels).item()
+        epoch_loss += loss
+        optimizer.zero_grad(); loss.backward(); optimizer.step()
+        
         
     return epoch_loss / len(data_loader)
 
@@ -49,20 +55,21 @@ def train(config):
     model = OurModel(config)
 
     # Optimizer and loss function initialization
-
+    optimizer = optim.Adam(model.parameters(), lr=config["learning_rate"])
+    criterion = nn.CrossEntropyLoss()
+    
     # Training loop
     for epoch in range(num_epochs):
         start_time = time.time()
 
         # Epoch training step
-        train_loss = train_epoch(model, train_set, batch_size, config["use_cuda"])
+        train_loss = train_epoch(model, criterion, optimizer, train_set, batch_size, config["use_cuda"])
         
         end_time = time.time()
         epoch_mins, epoch_secs = epoch_time(start_time, end_time)
 
         # Compare losses and store model if better
-    
-        print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+        print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s | Train loss: {train_loss:0.2}')
 
 
 if __name__ == "__main__":
